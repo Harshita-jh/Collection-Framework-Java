2ed83c50a05802caf7a9001735161424402ac9de
Harshita <harshita4420@gmail.com>
2023-07-06 11:19:10 +0530

    Adding Map

---
+++ HashMapDemo.java
@@ -0,0 +1,10 @@
+package collectionframe;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.
+    }
+}
--- PriorityQueueDemo.java
+++ PriorityQueueDemo.java
@@ -1,7 +1,26 @@
 package collectionframe;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-        
+        //default first element will be minimum value
+        //  PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //  pq.offer(12);
+        //  pq.add(3);
+        //  pq.add(4);
+        //  pq.offer(2);
+        //  System.out.println("PriorityQueue = "+pq);
+        //  System.out.println("PriorityQueue = "+pq.peek());
+
+        // but using Comparator.reverseOrder() first element will be largest value
+         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+         pq.offer(12);
+         pq.add(3);
+         pq.add(4);
+         pq.offer(2);
+         System.out.println("PriorityQueue = "+pq);
+         System.out.println("PriorityQueue = "+pq.peek());
     }
 }
